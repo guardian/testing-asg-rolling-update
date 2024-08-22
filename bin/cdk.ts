b8fb75f14fe620e42156da69db25dcb9275cdbca
akash1810 <akash1810@users.noreply.github.com>
2024-08-22 18:09:24 +0100

    Initialise a new GuCDK project

---
+++ bin/cdk.ts
@@ -0,0 +1,10 @@
+import 'source-map-support/register';
+import { GuRoot } from '@guardian/cdk/lib/constructs/root';
+import { TestingAsgRollingUpdate } from '../lib/testing-asg-rolling-update';
+
+const app = new GuRoot();
+new TestingAsgRollingUpdate(app, 'TestingAsgRollingUpdate-euwest-1-CODE', {
+	stack: 'playground',
+	stage: 'CODE',
+	env: { region: 'eu-west-1' },
+});
---
+++ lib/testing-asg-rolling-update.test.ts
@@ -0,0 +1,15 @@
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { TestingAsgRollingUpdate } from './testing-asg-rolling-update';
+
+describe('The TestingAsgRollingUpdate stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const stack = new TestingAsgRollingUpdate(app, 'TestingAsgRollingUpdate', {
+			stack: 'playground',
+			stage: 'TEST',
+		});
+		const template = Template.fromStack(stack);
+		expect(template.toJSON()).toMatchSnapshot();
+	});
+});
